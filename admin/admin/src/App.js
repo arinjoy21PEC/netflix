e0176a9fd4e61285e0a5abc29d63e6cb3c8034f0
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-05 22:15:52 +0530

    Update App.js

--- admin/admin/src/App.js
+++ admin/admin/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./context/authContext/AuthContext";
 import { useContext } from "react";
-import { AuthContext } from "./authContext/AuthContext";
 
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
 import "./App.css";
-import Home from "./Pages/Home/Home";
-import Watch from "./Pages/Watch/Watch";
-import Login from "./Pages/Login/Login";
-
+import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
+import Login from "./pages/Login/Login";
+import ListList from "./pages/listList/ListList";
+import List from "./pages/list/List";
+import NewList from "./pages/newList/NewList";
 const App = () => {
   const { user } = useContext(AuthContext);
 
@@ -24,9 +33,15 @@ const App = () => {
         />
         {user && (
           <>
-            <Route path="/movies" element={<Home type="movie" />} />
-            <Route path="/series" element={<Home type="series" />} />
-            <Route path="/watch" element={<Watch />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
+          <Route path="/newUser" element={<NewUser />} />
+          <Route path="/movies" element={<ProductList />} />
+          <Route path="/movies/find/:productId" element={<Product />} />
+          <Route path="/newProduct" element={<NewProduct />} />
+          <Route path="/lists" element={<ListList />} />
+          <Route path="/list/:listId" element={<List />} />
+          <Route path="/newlist" element={<NewList />} />
           </>
         )}
         <Route path="*" element={<Navigate to="/" replace />} />
