8e8dff31ddc6829dfafcf3a41d32278da941d40f
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-05 22:05:21 +0530

    Update WidgetSm.jsx

--- admin/admin/src/components/widgetSm/WidgetSm.jsx
+++ admin/admin/src/components/widgetSm/WidgetSm.jsx
@@ -2,63 +2,48 @@ import { VisibilityOutlined } from "@mui/icons-material";
 import "./widgetSm.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API,
-});
+})
 
-export default function WidgetSm() {
-  const [newUsers, setNewUsers] = useState([]);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const getNewUsers = async () => {
+export default function WidgetSm(){
+  const [newUsers, setNewUsers]=useState([])
+  useEffect(()=>{
+    const getNewUsers = async ()=>{
       try {
-        const user = JSON.parse(localStorage.getItem("user"));
-
-        if (!user || !user.accessToken) {
-          // Handle missing or expired token, redirect to login page
-          navigate("/login");
-          return;
-        }
-
-        const res = await axiosInstance.get("/users?new=true", {
-          headers: {
-            token: "Bearer " + user.accessToken,
-          },
+        const res = await axiosInstance.get("/users?new=true",
+        {
+          headers:{
+            token:"Bearer " +JSON.parse(localStorage.getItem("user")).accessToken,
+          }
         });
         setNewUsers(res.data);
       } catch (err) {
-        console.log(err);
+        console.log(err)
       }
-    };
-
+    }
     getNewUsers();
-  }, [navigate]);
-
+  },[])
   return (
     <div className="widgetSm">
       <span className="widgetSmTitle">New Join Members</span>
       <ul className="widgetSmList">
-        {newUsers.map((user) => (
+        {newUsers.map((user)=>(
           <li key={user._id} className="widgetSmListItem">
-            <img
-              src={
-                user.profilePic ||
-                "https://ih1.redbubble.net/image.618427277.3222/flat,800x800,075,f.u2.jpg"
-              }
-              alt=""
-              className="widgetSmImg"
+          <img
+            src={user.profilePic || "https://ih1.redbubble.net/image.618427277.3222/flat,800x800,075,f.u2.jpg"}
+            alt=""
+            className="widgetSmImg"
             />
-            <div className="widgetSmUser">
-              <span className="widgetSmUsername">{user.username}</span>
-            </div>
-            <button className="widgetSmButton">
-              <VisibilityOutlined className="widgetSmIcon" />
-              Display
-            </button>
-          </li>
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">{user.username}</span>
+          </div>
+          <button className="widgetSmButton">
+            <VisibilityOutlined className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
         ))}
       </ul>
     </div>
