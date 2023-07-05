ced5a9c84ab297944501924d305a0d568b4c0913
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-05 21:25:34 +0530

    Update Home.jsx

--- admin/admin/src/pages/home/Home.jsx
+++ admin/admin/src/pages/home/Home.jsx
@@ -6,61 +6,78 @@ import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
+
 import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+
 export default function Home() {
-  const MONTHS= useMemo(()=>[
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ],
-  []
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
   );
-  const [userStats, setUserStats]=useState([]);
+  const [userStats, setUserStats] = useState([]);
   const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API,
-  })
-  
-  useEffect(()=>{
-    const getStats = async ()=>{
-      try{
-        const res = await axiosInstance.get("/users/stats", 
-        {
-          headers:{
-            token:"Bearer "+JSON.parse(localStorage.getItem("user")).accessToken
+  });
+  const history = useHistory();
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await axiosInstance.get("/users/stats", {
+          headers: {
+            token:
+              "Bearer " +
+              JSON.parse(localStorage.getItem("user")).accessToken,
           },
-        }); 
-        const statsList = res.data.sort(function (a,b){
+        });
+
+        if (res.data.error === "Token expired") {
+          // Handle token expiration, redirect to login page
+          localStorage.removeItem("user");
+          history.push("/login");
+          return;
+        }
+
+        const statsList = res.data.sort(function (a, b) {
           return a._id - b._id;
-        })
-        res.data.map((item)=>
-        setUserStats((prev)=> [
-          ...prev,
-          {name:MONTHS[item._id-1], "New User": item.total },
-        ])
-      );
-      }catch(err){
-        console.log(err); 
+        });
+
+        res.data.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "New User": item.total },
+          ])
+        );
+      } catch (err) {
+        console.log(err);
       }
     };
+
     getStats();
-  },[MONTHS])
+  }, [MONTHS, axiosInstance, history]);
+
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart data={userStats} title="User Analytics" grid dataKey="New User"/>
+      <Chart data={userStats} title="User Analytics" grid dataKey="New User" />
       <div className="homeWidgets">
-        <WidgetSm/>
-        <WidgetLg/>
+        <WidgetSm />
+        <WidgetLg />
       </div>
     </div>
   );
