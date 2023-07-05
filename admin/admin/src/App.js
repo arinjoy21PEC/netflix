e64e9af323af24a5468bd8d4ffbe305ae2d619cc
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-05 22:02:25 +0530

    Update App.js

--- admin/admin/src/App.js
+++ admin/admin/src/App.js
@@ -1,68 +1,38 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { AuthContext } from "./context/authContext/AuthContext";
 import { useContext } from "react";
+import { AuthContext } from "./authContext/AuthContext";
 
-import Sidebar from "./components/sidebar/Sidebar";
-import Topbar from "./components/topbar/Topbar";
-import "./App.css";
-import Home from "./pages/home/Home";
-import UserList from "./pages/userList/UserList";
-import User from "./pages/user/User";
-import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/ProductList";
-import Product from "./pages/product/Product";
-import NewProduct from "./pages/newProduct/NewProduct";
-import Login from "./pages/Login/Login";
-import ListList from "./pages/listList/ListList";
-import List from "./pages/list/List";
-import NewList from "./pages/newList/NewList";
+import "./app.scss";
+import Home from "./Pages/Home/Home";
+import Watch from "./Pages/Watch/Watch";
+import Login from "./Pages/Login/Login";
 
-function App() {
+const App = () => {
   const { user } = useContext(AuthContext);
 
   return (
     <Router>
-      {user ? (
-        <AuthenticatedRoutes />
-      ) : (
-        <AuthenticatedRoutes />
-      )}
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" replace />}
+        />
+        {user && (
+          <>
+            <Route path="/movies" element={<Home type="movie" />} />
+            <Route path="/series" element={<Home type="series" />} />
+            <Route path="/watch" element={<Watch />} />
+          </>
+        )}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </Router>
   );
-}
-
-function AuthenticatedRoutes() {
-  return (
-    <>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<UserList />} />
-          <Route path="/user/:userId" element={<User />} />
-          <Route path="/newUser" element={<NewUser />} />
-          <Route path="/movies" element={<ProductList />} />
-          <Route path="/movies/find/:productId" element={<Product />} />
-          <Route path="/newProduct" element={<NewProduct />} />
-          <Route path="/lists" element={<ListList />} />
-          <Route path="/list/:listId" element={<List />} />
-          <Route path="/newlist" element={<NewList />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
-    </>
-  );
-}
-
-function UnauthenticatedRoutes() {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
-  );
-}
+};
 
 export default App;
