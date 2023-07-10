d20544f68d4f8efe672e27de9a7fa3814c0468f1
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-10 20:09:57 +0530

    Update Navbar.jsx

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
       <div className="container">
         <div className="left">
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            src="https://drive.google.com/file/d/150qDAm4XaV41iN0StTtG_H9njFjIGJxh/view?usp=drive_link"
             alt=""
           />
           <Link to="/" className="link">
