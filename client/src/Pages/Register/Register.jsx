dfa068e3a8d67c51b24e9eb00953d108f3c1a8d4
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-10 20:27:20 +0530

    Update Register.jsx

--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -40,7 +40,7 @@ export default function Register() {
         <div className="wrapper">
           <img
             className="logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            src="./antflix.png"
             alt=""
           />
             {/* <button  className="loginButton" 
