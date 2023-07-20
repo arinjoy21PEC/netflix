1ed92f7b81e54bc7095228143e255b8d4aa4f879
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-20 21:03:29 +0530

    Update App.js

--- admin/admin/src/App.js
+++ admin/admin/src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { AuthContext } from "./context/authContext/AuthContext";
-import { useContext } from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import "./App.css";
 import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
@@ -13,55 +10,83 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
-import Login from "./pages/Login/Login";
 import ListList from "./pages/listList/ListList";
 import List from "./pages/list/List";
 import NewList from "./pages/newList/NewList";
+import Login from "./pages/Login/Login";
 
-function App() {
-  const { user } = useContext(AuthContext);
+import "./App.css";
 
-  return (
-    <Router>
-      {user ? (
-        <AuthenticatedRoutes />
-      ) : (
-        <AuthenticatedRoutes />
-      )}
-    </Router>
-  );
-}
-
-function AuthenticatedRoutes() {
+const Layout = () => {
   return (
     <>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<UserList />} />
-          <Route path="/user/:userId" element={<User />} />
-          <Route path="/newUser" element={<NewUser />} />
-          <Route path="/movies" element={<ProductList />} />
-          <Route path="/movies/find/:productId" element={<Product />} />
-          <Route path="/newProduct" element={<NewProduct />} />
-          <Route path="/lists" element={<ListList />} />
-          <Route path="/list/:listId" element={<List />} />
-          <Route path="/newlist" element={<NewList />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <Outlet />
       </div>
     </>
   );
-}
+};
 
-function UnauthenticatedRoutes() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/users",
+        element: <UserList />
+      },
+      {
+        path: "/user/:userId",
+        element: <User />
+      },
+      {
+        path: "/newUser",
+        element: <NewUser />
+      },
+      {
+        path: "/movies",
+        element: <ProductList />
+      },
+      {
+        path: "/movies/find/:productId",
+        element: <Product />
+      },
+      {
+        path: "/newProduct",
+        element: <NewProduct />
+      },
+      {
+        path: "/lists",
+        element: <ListList />
+      },
+      {
+        path: "/list/:listId",
+        element: <List />
+      },
+      {
+        path: "/newlist",
+        element: <NewList />
+      },
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+]);
+
+function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
