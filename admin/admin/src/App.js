68ec56a1448b39a62377ebc2e5bfbdb58f73d104
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-20 21:06:17 +0530

    Update App.js

--- admin/admin/src/App.js
+++ admin/admin/src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-
+import { createBrowserRouter, RouterProvider, Outlet, RouteObject } from "react-router-dom";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import "./app.css";
 import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/ProductList";
-import Product from "./pages/product/Product";
-import NewProduct from "./pages/newProduct/NewProduct";
+import Login from "./pages/login/Login";
+import { AuthContext } from "./context/authContext/AuthContext";
+import { useContext } from "react";
 import ListList from "./pages/listList/ListList";
 import List from "./pages/list/List";
 import NewList from "./pages/newList/NewList";
-import Login from "./pages/Login/Login";
-
-import "./App.css";
+import MovieList from "./pages/movieList/MovieList";
+import Movie from "./pages/movie/Movie";
+import NewMovie from "./pages/newMovie/NewMovie";
 
 const Layout = () => {
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
+    return <Login />;
+  }
+
   return (
     <>
       <Topbar />
@@ -29,58 +35,60 @@ const Layout = () => {
   );
 };
 
-const router = createBrowserRouter([
+const routes: RouteObject[] = [
+  {
+    path: "/login",
+    element: <Login />,
+  },
   {
     path: "/",
     element: <Layout />,
     children: [
       {
         path: "/",
-        element: <Home />
+        element: <Home />,
       },
       {
         path: "/users",
-        element: <UserList />
+        element: <UserList />,
       },
       {
         path: "/user/:userId",
-        element: <User />
+        element: <User />,
       },
       {
         path: "/newUser",
-        element: <NewUser />
+        element: <NewUser />,
       },
       {
         path: "/movies",
-        element: <ProductList />
+        element: <MovieList />,
       },
       {
-        path: "/movies/find/:productId",
-        element: <Product />
+        path: "/movie/:movieId",
+        element: <Movie />,
       },
       {
-        path: "/newProduct",
-        element: <NewProduct />
+        path: "/newMovie",
+        element: <NewMovie />,
       },
       {
         path: "/lists",
-        element: <ListList />
+        element: <ListList />,
       },
       {
         path: "/list/:listId",
-        element: <List />
+        element: <List />,
       },
       {
         path: "/newlist",
-        element: <NewList />
+        element: <NewList />,
       },
-    ]
+    ],
   },
-  {
-    path: "/login",
-    element: <Login />
-  },
-]);
+];
+
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
