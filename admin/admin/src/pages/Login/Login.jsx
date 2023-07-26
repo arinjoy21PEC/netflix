25293fff4e116f268b8092def659b4047772d138
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-27 00:48:20 +0530

    Update Login.jsx

--- admin/admin/src/pages/Login/Login.jsx
+++ admin/admin/src/pages/Login/Login.jsx
@@ -9,7 +9,7 @@ import { useContext } from 'react';
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { isFetching, dispatch } = useContext(AuthContext);
+  const { dispatch } = useContext(AuthContext);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -21,7 +21,7 @@ const Login = () => {
         <form className='loginForm'>
             <input type="text" placeholder='email' className='loginInput' onChange={(e) => setEmail(e.target.value)}/>
             <input type="password" placeholder='password' className='loginInput' onChange={(e) => setPassword(e.target.value)}/>
-            <button className="loginButton" onClick={handleLogin} disabled={isFetching}>Login</button>
+            <button className="loginButton" onClick={handleLogin}>Login</button>
         </form>
     </div>
   )
