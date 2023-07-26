604df44e5c1f170482feccca490c96c9f596209e
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-27 01:03:48 +0530

    Update Login.jsx

--- admin/admin/src/pages/Login/Login.jsx
+++ admin/admin/src/pages/Login/Login.jsx
@@ -1,30 +1,39 @@
-import React from 'react'
-import "./login.css"
+import React from 'react';
 import { useState } from 'react';
 import { login } from "../../context/authContext/apiCalls";
 import { AuthContext } from "../../context/authContext/AuthContext";
 import { useContext } from 'react';
-
+import { useHistory } from 'react-router-dom'; // Import useHistory hook
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { dispatch } = useContext(AuthContext);
+  const history = useHistory(); // Get the history object from useHistory
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    login({ email, password }, dispatch);
+    try {
+      const response = await login({ email, password }, dispatch);
+      if (response.success) {
+        // If login is successful, navigate to a different page
+        history.push('/'); // Replace 'dashboard' with the desired page URL
+      }
+    } catch (error) {
+      // Handle login error if needed
+      console.log(error);
+    }
   };
 
   return (
     <div className='login'>
-        <form className='loginForm'>
-            <input type="text" placeholder='email' className='loginInput' onChange={(e) => setEmail(e.target.value)}/>
-            <input type="password" placeholder='password' className='loginInput' onChange={(e) => setPassword(e.target.value)}/>
-            <button className="loginButton" onClick={handleLogin}>Login</button>
-        </form>
+      <form className='loginForm'>
+        <input type="text" placeholder='email' className='loginInput' onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder='password' className='loginInput' onChange={(e) => setPassword(e.target.value)} />
+        <button className="loginButton" onClick={handleLogin}>Login</button>
+      </form>
     </div>
   )
 }
 
-export default Login
+export default Login;
