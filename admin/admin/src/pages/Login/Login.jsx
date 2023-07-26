eef91e483539d50cf9c3eabc494e1414ea91a72a
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-27 01:39:58 +0530

    Update Login.jsx

--- admin/admin/src/pages/Login/Login.jsx
+++ admin/admin/src/pages/Login/Login.jsx
@@ -1,40 +1,30 @@
-import React from 'react';
+import React from 'react'
+import "./login.css"
 import { useState } from 'react';
 import { login } from "../../context/authContext/apiCalls";
 import { AuthContext } from "../../context/authContext/AuthContext";
 import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import "./login.css"
+
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { dispatch } = useContext(AuthContext);
-  const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
-    try {
-      const response = await login({ email, password }, dispatch);
-      console.log(response); // Log the response to see its content
-      if (response && response.success){
-        navigate('/');
-      }
-    } catch (error) {
-      // Handle login error if needed
-      console.log(error);
-    }
+    login({ email, password }, dispatch);
   };
 
   return (
     <div className='login'>
-      <form className='loginForm'>
-        <input type="text" placeholder='email' className='loginInput' onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder='password' className='loginInput' onChange={(e) => setPassword(e.target.value)} />
-        <button className="loginButton" onClick={handleLogin}>Login</button>
-      </form>
+        <form className='loginForm'>
+            <input type="text" placeholder='email' className='loginInput' onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" placeholder='password' className='loginInput' onChange={(e) => setPassword(e.target.value)}/>
+            <button className="loginButton" onClick={handleLogin}>Login</button>
+        </form>
     </div>
   )
 }
 
-export default Login;
+export default Login
